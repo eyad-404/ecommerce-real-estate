a3d2640146f84185b6b6a82cd572b2697d70fa78
eyad-404 <eyadops.ws@gmail.com>
2025-12-02 14:13:30 +0200

    Update ResetPasswordBrevo.php

--- app/Notifications/ResetPasswordBrevo.php
+++ app/Notifications/ResetPasswordBrevo.php
@@ -2,52 +2,36 @@
 
 namespace App\Notifications;
 
-use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Notification;
 use Illuminate\Notifications\Messages\MailMessage;
 use Brevo\Client\Api\TransactionalEmailsApi;
 use Brevo\Client\Configuration;
 use Brevo\Client\Model\SendSmtpEmail;
 
-class ResetPasswordBrevo extends Notification implements ShouldQueue
+class ResetPasswordBrevo extends Notification
 {
-    use Queueable;
-
     public $token;
 
-    /**
-     * Create a new notification instance.
-     */
     public function __construct($token)
     {
         $this->token = $token;
     }
 
-    /**
-     * Get the notification's delivery channels.
-     */
     public function via($notifiable)
     {
         // لازم Laravel Mail facade يكون موجود عشان Notification
         return ['mail'];
     }
 
-    /**
-     * Send the email using Brevo API.
-     */
     public function toMail($notifiable)
     {
-        // URL لإعادة تعيين كلمة المرور
         $resetUrl = url("/reset-password/{$this->token}?email=".urlencode($notifiable->email));
 
-        // إعداد Brevo API
         $config = Configuration::getDefaultConfiguration()
             ->setApiKey('api-key', env('BREVO_API_KEY'));
 
         $apiInstance = new TransactionalEmailsApi(new \GuzzleHttp\Client(), $config);
 
-        // محتوى الرسالة
         $sendSmtpEmail = new SendSmtpEmail([
             'subject' => 'Reset Your Password - EL Kayan',
             'sender' => [
@@ -61,7 +45,6 @@ class ResetPasswordBrevo extends Notification implements ShouldQueue
                               <p>Click <a href='{$resetUrl}'>here</a> to reset your password.</p>"
         ]);
 
-        // محاولة إرسال البريد
         try {
             $response = $apiInstance->sendTransacEmail($sendSmtpEmail);
             \Log::info('Brevo email sent successfully', [
@@ -74,7 +57,6 @@ class ResetPasswordBrevo extends Notification implements ShouldQueue
             ]);
         }
 
-        // MailMessage dummy عشان Laravel Notification
         return (new MailMessage)
             ->subject('Password Reset')
             ->line('If you are seeing this message, check the logs for Brevo API send.');
